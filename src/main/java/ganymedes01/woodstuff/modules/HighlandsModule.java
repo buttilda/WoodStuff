1a18efb34e0151231ecc05a75a5c4dbac2e85e30
ganymedes01 <foka_12@hotmail.com>
2015-06-14 20:53:38 -0300

    Highlands planks metadata only goes up to 3

--- src/main/java/ganymedes01/woodstuff/modules/HighlandsModule.java
+++ src/main/java/ganymedes01/woodstuff/modules/HighlandsModule.java
@@ -11,7 +11,7 @@ public class HighlandsModule extends WoodModule {
 	@Override
 	public void addBlocks() {
 		Block block = (Block) Block.blockRegistry.getObject("Highlands:hl_woodPlanks");
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 4; i++)
 			addWood(block, i, true, true, true, true, true);
 	}
 }
