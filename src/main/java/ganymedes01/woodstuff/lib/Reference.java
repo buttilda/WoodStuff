c77ca4f19bc259c55ba4628d0043a713e7462493
ganymedes01 <foka_12@hotmail.com>
2015-08-21 16:55:59 +0100

    Bump version

--- src/main/java/ganymedes01/woodstuff/lib/Reference.java
+++ src/main/java/ganymedes01/woodstuff/lib/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MOD_ID = "woodstuff";
 	public static final String MOD_NAME = "WoodStuff";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1448,);";
-	public static final String VERSION_NUMBER = "1.0.3";
+	public static final String VERSION_NUMBER = "1.1.0";
 
 	public static final String ITEM_BLOCK_TEXTURE_PATH = MOD_ID + ":";
 	public static final String ENTITY_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/entities/";
