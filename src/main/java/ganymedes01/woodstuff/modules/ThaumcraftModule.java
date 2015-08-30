7785445e65aeaeb819135fa1097c7341d2f677e1
ganymedes01 <foka_12@hotmail.com>
2015-08-30 17:25:19 +0100

    Added support for Thaumcraft's aspects. Closes #11

--- src/main/java/ganymedes01/woodstuff/modules/ThaumcraftModule.java
+++ src/main/java/ganymedes01/woodstuff/modules/ThaumcraftModule.java
@@ -1,6 +1,9 @@
 package ganymedes01.woodstuff.modules;
 
 import net.minecraft.block.Block;
+import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
 
 public class ThaumcraftModule extends WoodModule {
 
@@ -13,5 +16,17 @@ public class ThaumcraftModule extends WoodModule {
 		Block block = (Block) Block.blockRegistry.getObject("Thaumcraft:blockWoodenDevice");
 		addWood(block, 6, true, true, true, true, true, true, true);
 		addWood(block, 7, true, true, true, true, true, true, true);
+
+		addAspects();
+	}
+
+	private void addAspects() {
+		ThaumcraftApi.registerObjectTag("buttonWood", new AspectList().add(Aspect.MECHANISM, 1));
+		ThaumcraftApi.registerObjectTag("bookshelfWood", new AspectList().add(Aspect.TREE, 4).add(Aspect.MIND, 8).add(Aspect.CLOTH, 2));
+		ThaumcraftApi.registerObjectTag("fenceWood", new AspectList().add(Aspect.TREE, 2));
+		ThaumcraftApi.registerObjectTag("fencegateWood", new AspectList().add(Aspect.TREE, 4).add(Aspect.TRAVEL, 1).add(Aspect.MECHANISM, 1));
+		ThaumcraftApi.registerObjectTag("pressureplateWood", new AspectList().add(Aspect.TREE, 1).add(Aspect.SENSES, 1).add(Aspect.MECHANISM, 1));
+		ThaumcraftApi.registerObjectTag("chestWood", new AspectList().add(Aspect.TREE, 6).add(Aspect.VOID, 4));
+		ThaumcraftApi.registerObjectTag("craftingTableWood", new AspectList().add(Aspect.CRAFT, 4));
 	}
 }
