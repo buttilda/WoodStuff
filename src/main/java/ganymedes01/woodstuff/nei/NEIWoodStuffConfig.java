a47567387e10cc481d5c998b41abeee8f1c85deb
ganymedes01 <foka_12@hotmail.com>
2015-10-20 13:48:59 +0100

    Implement NEI's [?] button for the crafting tables

---
+++ src/main/java/ganymedes01/woodstuff/nei/NEIWoodStuffConfig.java
@@ -0,0 +1,26 @@
+package ganymedes01.woodstuff.nei;
+
+import ganymedes01.woodstuff.gui.GuiCraftingTable;
+import ganymedes01.woodstuff.lib.Reference;
+import codechicken.nei.api.API;
+import codechicken.nei.api.IConfigureNEI;
+import codechicken.nei.recipe.DefaultOverlayHandler;
+
+public class NEIWoodStuffConfig implements IConfigureNEI {
+
+	@Override
+	public String getName() {
+		return Reference.MOD_NAME;
+	}
+
+	@Override
+	public String getVersion() {
+		return Reference.VERSION_NUMBER;
+	}
+
+	@Override
+	public void loadConfig() {
+		API.registerGuiOverlay(GuiCraftingTable.class, "crafting");
+		API.registerGuiOverlayHandler(GuiCraftingTable.class, new DefaultOverlayHandler(), "crafting");
+	}
+}
