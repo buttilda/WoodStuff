16b8e3a8a2b5b0df25b85a1efa5d78bc3bb26953
ganymedes01 <foka_12@hotmail.com>
2016-01-07 13:21:58 +0000

    Bump version

--- src/main/java/ganymedes01/woodstuff/lib/Reference.java
+++ src/main/java/ganymedes01/woodstuff/lib/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MOD_ID = "woodstuff";
 	public static final String MOD_NAME = "WoodStuff";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1558,);after:arsmagica2";
-	public static final String VERSION_NUMBER = "1.3.1";
+	public static final String VERSION_NUMBER = "1.3.2";
 
 	public static final String ITEM_BLOCK_TEXTURE_PATH = MOD_ID + ":";
 	public static final String ENTITY_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/entities/";
